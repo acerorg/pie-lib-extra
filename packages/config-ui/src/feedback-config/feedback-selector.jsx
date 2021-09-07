765e6869db10e80c7e06a98d391101cd88637c3f
alextkd <lazar.alexandru.emil@gmail.com>
2021-09-07 22:13:04 +0300

    pie: update version for config-ui

--- packages/config-ui/src/feedback-config/feedback-selector.jsx
+++ packages/config-ui/src/feedback-config/feedback-selector.jsx
@@ -1,4 +1,4 @@
-import EditableHTML from '@teachforward/editable-html';
+import EditableHTML from '@teachforward/editable-html-tm';
 import { InputContainer } from '@pie-lib/render-ui';
 import PropTypes from 'prop-types';
 import React from 'react';
